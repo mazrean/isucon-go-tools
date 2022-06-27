2d74724295601c90b171e8298360b2e6f361e00d
mazrean <shunsuke.w04@gmail.com>
2022-06-28 04:21:26 +0900

    all purge cache

---
+++ cache/sc.go
@@ -0,0 +1,86 @@
+package cache
+
+import (
+	"context"
+	"time"
+
+	isutools "github.com/mazrean/isucon-go-tools"
+	"github.com/motoki317/sc"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+const (
+	prometheusNamespace = "isutools"
+	prometheusSubsystem = "cache"
+)
+
+var (
+	cacheMap = make(map[string]interface {
+		Purge()
+	}, 0)
+)
+
+func New[K comparable, V any](name string, replaceFn func(ctx context.Context, key K) (V, error), freshFor, ttl time.Duration, options ...sc.CacheOption) (*sc.Cache[K, V], error) {
+	cache, err := sc.New(replaceFn, freshFor, ttl, options...)
+	if err != nil {
+		return cache, err
+	}
+
+	cacheMap[name] = cache
+
+	if isutools.Enable {
+		promauto.NewGaugeFunc(prometheus.GaugeOpts{
+			Namespace: prometheusNamespace,
+			Subsystem: prometheusSubsystem,
+			Name:      "hit_count",
+			ConstLabels: prometheus.Labels{
+				"name": name,
+				"stat": "hit",
+			},
+		}, func() float64 {
+			return float64(cache.Stats().Hits)
+		})
+		promauto.NewGaugeFunc(prometheus.GaugeOpts{
+			Namespace: prometheusNamespace,
+			Subsystem: prometheusSubsystem,
+			Name:      "hit_count",
+			ConstLabels: prometheus.Labels{
+				"name": name,
+				"stat": "grace_hit",
+			},
+		}, func() float64 {
+			return float64(cache.Stats().GraceHits)
+		})
+		promauto.NewGaugeFunc(prometheus.GaugeOpts{
+			Namespace: prometheusNamespace,
+			Subsystem: prometheusSubsystem,
+			Name:      "hit_count",
+			ConstLabels: prometheus.Labels{
+				"name": name,
+				"stat": "miss",
+			},
+		}, func() float64 {
+			return float64(cache.Stats().Misses)
+		})
+		promauto.NewGaugeFunc(prometheus.GaugeOpts{
+			Namespace: prometheusNamespace,
+			Subsystem: prometheusSubsystem,
+			Name:      "hit_count",
+			ConstLabels: prometheus.Labels{
+				"name": name,
+				"stat": "replace",
+			},
+		}, func() float64 {
+			return float64(cache.Stats().Replacements)
+		})
+	}
+
+	return cache, nil
+}
+
+func AllPurge() {
+	for _, cache := range cacheMap {
+		cache.Purge()
+	}
+}
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/motoki317/sc v1.4.2
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.32.1 // indirect
 	github.com/prometheus/procfs v0.7.3 // indirect
