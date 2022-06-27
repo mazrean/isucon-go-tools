24693af0f96bfd9af82d0ea54ceb1c3ead52513b
mazrean <shunsuke.w04@gmail.com>
2022-06-28 05:22:31 +0900

    map pool

--- pool/pool.go
+++ pool/pool.go
@@ -110,3 +110,56 @@ func (p *SlicePool[T, _]) Put(t T) {
 
 	p.pool.Put(t)
 }
+
+type MapPool[T ~*map[K]S, K comparable, S any] struct {
+	pool    *sync.Pool
+	name    string
+	counter *prometheus.CounterVec
+}
+
+func NewMap[T ~*map[K]S, K comparable, S any](name string, fn func() T) *MapPool[T, K, S] {
+	var counter *prometheus.CounterVec
+	if isutools.Enable {
+		counter = promauto.NewCounterVec(prometheus.CounterOpts{
+			Namespace: prometheusNamespace,
+			Subsystem: prometheusSubsystem,
+			Name:      "count",
+		}, []string{"name", "type"})
+	}
+
+	return &MapPool[T, K, S]{
+		pool: &sync.Pool{
+			New: func() interface{} {
+				if counter != nil {
+					counter.WithLabelValues(name, "alloc").Inc()
+				}
+
+				return fn()
+			},
+		},
+		name:    name,
+		counter: counter,
+	}
+}
+
+func (p *MapPool[T, _, _]) Get() T {
+	if p.counter != nil {
+		p.counter.WithLabelValues(p.name, "get").Inc()
+	}
+
+	// 全てのデータを消去する
+	v := p.pool.Get().(T)
+	for k := range *v {
+		delete(*v, k)
+	}
+
+	return v
+}
+
+func (p *MapPool[T, _, _]) Put(t T) {
+	if p.counter != nil {
+		p.counter.WithLabelValues(p.name, "put").Inc()
+	}
+
+	p.pool.Put(t)
+}
