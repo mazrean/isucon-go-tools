11a87cde5ad1a3c6cf6177b2103a2efa9091920e
mazrean <shunsuke.w04@gmail.com>
2022-07-06 16:09:45 +0900

    cacheにMap追加

--- cache/sc.go
+++ cache/sc.go
@@ -2,6 +2,7 @@ package isucache
 
 import (
 	"context"
+	"sync"
 	"time"
 
 	isutools "github.com/mazrean/isucon-go-tools"
@@ -77,6 +78,118 @@ func New[K comparable, V any](name string, replaceFn func(ctx context.Context, k
 	}
 
 	return cache, nil
+}
+
+type Map[K comparable, V any] struct {
+	m            map[K]V
+	locker       sync.RWMutex
+	loadMetrics  *prometheus.GaugeVec
+	storeMetrics *prometheus.GaugeVec
+}
+
+func NewMap[K comparable, V any](name string) *Map[K, V] {
+	var (
+		loadMetrics  *prometheus.GaugeVec
+		storeMetrics *prometheus.GaugeVec
+	)
+	if isutools.Enable {
+		loadMetrics = promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: prometheusNamespace,
+			Subsystem: prometheusSubsystem,
+			Name:      "load_count",
+			ConstLabels: prometheus.Labels{
+				"name": name,
+			},
+		}, []string{"hit", "miss"})
+
+		storeMetrics = promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: prometheusNamespace,
+			Subsystem: prometheusSubsystem,
+			Name:      "store_count",
+			ConstLabels: prometheus.Labels{
+				"name": name,
+			},
+		}, []string{"replace", "new", "remove"})
+	}
+
+	m := &Map[K, V]{
+		m:            make(map[K]V),
+		locker:       sync.RWMutex{},
+		loadMetrics:  loadMetrics,
+		storeMetrics: storeMetrics,
+	}
+
+	cacheMap[name] = m
+
+	return m
+}
+
+func (m *Map[K, V]) Load(key K) (V, bool) {
+	m.locker.RLock()
+
+	v, ok := m.m[key]
+	if ok {
+		m.locker.RUnlock()
+
+		if m.loadMetrics != nil {
+			m.loadMetrics.WithLabelValues("hit").Inc()
+		}
+
+		return v, true
+	}
+	m.locker.RUnlock()
+
+	if m.loadMetrics != nil {
+		m.loadMetrics.WithLabelValues("miss").Inc()
+	}
+
+	return v, false
+}
+
+func (m *Map[K, V]) Store(key K, value V) {
+	if m.loadMetrics != nil {
+		m.locker.RLock()
+		_, ok := m.m[key]
+		m.locker.RUnlock()
+
+		if ok {
+			m.storeMetrics.WithLabelValues("replace").Inc()
+		} else {
+			m.storeMetrics.WithLabelValues("new").Inc()
+		}
+	}
+
+	m.locker.Lock()
+	m.m[key] = value
+	m.locker.Unlock()
+}
+
+func (m *Map[K, V]) Forget(key K) {
+	if m.storeMetrics != nil {
+		m.locker.RLock()
+		_, ok := m.m[key]
+		m.locker.RUnlock()
+
+		if ok {
+			m.storeMetrics.WithLabelValues("remove").Inc()
+		}
+	}
+
+	m.locker.Lock()
+	delete(m.m, key)
+	m.locker.Unlock()
+}
+
+func (m *Map[K, V]) Purge() {
+	if m.storeMetrics != nil {
+		m.storeMetrics.WithLabelValues("remove").Add(float64(len(m.m)))
+	}
+
+	m.locker.Lock()
+	for k := range m.m {
+		delete(m.m, k)
+	}
+	m.locker.Unlock()
 }
 
 func AllPurge() {
