1c9650c1b74f1090ab88a8b51e8dd737bbcef86d
mazrean <shunsuke.w04@gmail.com>
2022-07-10 15:11:47 +0900

    gin修正

--- analysers/gin/analyser.go
+++ analysers/gin/analyser.go
@@ -21,9 +21,11 @@ const (
 	apiPkgName         = "github.com/mazrean/isucon-go-tools/http"
 	apiPkgDefaultIdent = "isuhttp"
 	apiPrefix          = "Gin"
+	apiFuncName        = "GinNew"
 )
 
 var (
+	ginFuncNames   = []string{"New", "Default"}
 	ginMethodNames = []string{"Run", "RunTLS"}
 
 	importPkgs []*suggest.ImportInfo
@@ -37,6 +39,11 @@ var (
 )
 
 func run(pass *analysis.Pass) (any, error) {
+	err := wrapNew(pass)
+	if err != nil {
+		return nil, fmt.Errorf("failed to wrap gin.New: %w", err)
+	}
+
 	ssaGraph, ok := pass.ResultOf[buildssa.Analyzer].(*buildssa.SSA)
 	if !ok {
 		return nil, errors.New("failed to get ssa graph")
@@ -108,4 +115,132 @@ func run(pass *analysis.Pass) (any, error) {
 	}
 
 	return importPkgs, nil
+}
+
+func wrapNew(pass *analysis.Pass) error {
+	var pkgIdent string
+	for _, pkg := range pass.Pkg.Imports() {
+		if analysisutil.RemoveVendor(pkg.Path()) == ginPkgName {
+			pkgIdent = pkg.Name()
+			break
+		}
+	}
+	if len(pkgIdent) == 0 {
+		return nil
+	}
+
+	funcInfoList := []*funcInfo{}
+	for _, funcName := range ginFuncNames {
+		funcInfoList = append(funcInfoList, &funcInfo{
+			pkgName:  ginPkgName,
+			pkgIdent: pkgIdent,
+			funcName: funcName,
+		})
+	}
+
+	callInfoList := []*callInfo{}
+	for _, f := range pass.Files {
+		v := visitor{
+			funcInfoList: funcInfoList,
+		}
+
+		ast.Walk(&v, f)
+
+		if len(v.callExprs) != 0 {
+			importPkgs = append(importPkgs, &suggest.ImportInfo{
+				File:  f,
+				Ident: apiPkgDefaultIdent,
+				Path:  apiPkgName,
+			})
+
+			callInfoList = append(callInfoList, v.callExprs...)
+		}
+	}
+
+	if len(callInfoList) == 0 {
+		return nil
+	}
+
+	for _, callInfo := range callInfoList {
+		buf := bytes.Buffer{}
+
+		err := format.Node(&buf, pass.Fset, &ast.CallExpr{
+			Fun: &ast.SelectorExpr{
+				X:   ast.NewIdent(apiPkgDefaultIdent),
+				Sel: ast.NewIdent(apiFuncName),
+			},
+			Args: []ast.Expr{callInfo.expr},
+		})
+		if err != nil {
+			return fmt.Errorf("failed to format import declaration: %w", err)
+		}
+
+		pass.Report(analysis.Diagnostic{
+			Pos:     callInfo.expr.Pos(),
+			Message: fmt.Sprintf("should wrap (%s).%s with (%s).%s", callInfo.funcInfo.pkgName, callInfo.funcInfo.funcName, apiPkgName, apiFuncName),
+			SuggestedFixes: []analysis.SuggestedFix{{
+				Message: fmt.Sprintf("wrap (%s).%s with (%s).%s", callInfo.funcInfo.pkgName, callInfo.funcInfo.funcName, apiPkgName, apiFuncName),
+				TextEdits: []analysis.TextEdit{{
+					Pos:     callInfo.expr.Pos(),
+					End:     callInfo.expr.End(),
+					NewText: buf.Bytes(),
+				}},
+			}},
+		})
+	}
+
+	return nil
+}
+
+type funcInfo struct {
+	pkgName  string
+	pkgIdent string
+	funcName string
+}
+
+type callInfo struct {
+	funcInfo *funcInfo
+	expr     *ast.CallExpr
+}
+
+type visitor struct {
+	funcInfoList []*funcInfo
+	callExprs    []*callInfo
+}
+
+func (v *visitor) Visit(node ast.Node) ast.Visitor {
+	if node == nil {
+		return nil
+	}
+
+	switch expr := node.(type) {
+	case *ast.CallExpr:
+		calleeSelector, ok := expr.Fun.(*ast.SelectorExpr)
+		if !ok {
+			return v
+		}
+
+		selName, ok := calleeSelector.X.(*ast.Ident)
+		if !ok {
+			return v
+		}
+
+		if selName.Name == apiPkgDefaultIdent && calleeSelector.Sel.Name == apiFuncName {
+			return nil
+		}
+
+		for _, funcInfo := range v.funcInfoList {
+			if selName.Name == funcInfo.pkgIdent && calleeSelector.Sel.Name == funcInfo.funcName {
+				v.callExprs = append(v.callExprs, &callInfo{
+					funcInfo: funcInfo,
+					expr:     expr,
+				})
+				break
+			}
+		}
+
+		return v
+	}
+
+	return v
 }
--- http/gin.go
+++ http/gin.go
@@ -2,34 +2,69 @@ package isuhttp
 
 import (
 	"log"
-	"os"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	isutools "github.com/mazrean/isucon-go-tools"
 )
 
+func GinNew(engine *gin.Engine) *gin.Engine {
+	engine.Use(GinMetricsMiddleware)
+
+	return engine
+}
+
 func GinRun(engine *gin.Engine, addrs ...string) error {
-	var addr string
-	switch len(addrs) {
-	case 0:
-		port := os.Getenv("PORT")
-		if port != "" {
-			log.Printf("Environment variable PORT=\"%s\"\n", port)
-			addr = ":" + port
-			break
-		}
-		log.Println("Environment variable PORT is undefined. Using port :8080 by default")
-		addr = ":8080"
-	case 1:
-		addr = addrs[0]
-	default:
-		panic("too many parameters")
+	listener, ok, err := newUnixDomainSockListener()
+	if err != nil {
+		log.Printf("failed to init unix domain socket: %s\n", err)
 	}
 
-	// logに若干変化が出るが、ISUCON用ツールなので許容する
-	return ListenAndServe(addr, engine.Handler())
+	if ok {
+		return engine.RunListener(listener)
+	}
+
+	return engine.Run(addrs...)
 }
 
 func GinRunTLS(engine *gin.Engine, addr, certFile, keyFile string) error {
+	listener, ok, err := newUnixDomainSockListener()
+	if err != nil {
+		log.Printf("failed to init unix domain socket: %s\n", err)
+	}
+
+	if ok {
+		return engine.RunListener(listener)
+	}
+
 	// logに若干変化が出るが、ISUCON用ツールなので許容する
-	return ListenAndServeTLS(addr, certFile, keyFile, engine.Handler())
+	return engine.RunTLS(addr, certFile, keyFile)
+}
+
+func GinMetricsMiddleware(c *gin.Context) {
+	if !isutools.Enable {
+		c.Next()
+		return
+	}
+
+	path := c.FullPath()
+	host := c.Request.Host
+	method := c.Request.Method
+
+	reqSz := reqSize(c.Request)
+
+	start := time.Now()
+	c.Next()
+	reqDur := float64(time.Since(start)) / float64(time.Second)
+
+	statusCode := c.Writer.Status()
+	resSize := c.Writer.Size()
+
+	strStatusCode := strconv.Itoa(statusCode)
+
+	reqSizeHistogramVec.WithLabelValues(strStatusCode, method, path).Observe(reqSz)
+	reqDurHistogramVec.WithLabelValues(strStatusCode, method, path).Observe(reqDur)
+	reqCounterVec.WithLabelValues(strStatusCode, method, host, path).Inc()
+	resSizeHistogramVec.WithLabelValues(strStatusCode, method, path).Observe(float64(resSize))
 }
