b97c321814d8868bf448089a4f5c75d6f5a60b14
mazrean <shunsuke.w04@gmail.com>
2023-03-04 22:43:32 +0900

    add block/mutex profile

--- embed.go
+++ embed.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"runtime"
+	"strconv"
 
 	_ "net/http/pprof"
 
@@ -20,6 +22,26 @@ func init() {
 	addr, ok := os.LookupEnv("METRICS_ADDR")
 	if !ok {
 		addr = ":6060"
+	}
+
+	strBlockRate, ok := os.LookupEnv("BLOCK_RATE")
+	if ok {
+		blockRate, err := strconv.Atoi(strBlockRate)
+		if err != nil {
+			log.Printf("failed to parse BLOCK_RATE(%s): %v", strBlockRate, err)
+		} else {
+			runtime.SetBlockProfileRate(blockRate)
+		}
+	}
+
+	strMutexRate, ok := os.LookupEnv("MUTEX_RATE")
+	if ok {
+		mutexRate, err := strconv.Atoi(strMutexRate)
+		if err != nil {
+			log.Printf("failed to parse MUTEX_RATE(%s): %v", strMutexRate, err)
+		} else {
+			runtime.SetMutexProfileFraction(mutexRate)
+		}
 	}
 
 	http.Handle("/metrics", promhttp.Handler())
