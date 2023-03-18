bf305542419bc685fbafbbfdbe4e4c843d3c18cd
mazrean <shunsuke.w04@gmail.com>
2023-03-19 00:19:34 +0900

    B+ tree

---
+++ cache/bptree.go
@@ -0,0 +1,281 @@
+package isucache
+
+import (
+	"errors"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"golang.org/x/exp/constraints"
+)
+
+var treeHeightMetrics = promauto.NewGaugeVec(
+	prometheus.GaugeOpts{
+		Namespace: prometheusNamespace,
+		Subsystem: prometheusSubsystem,
+		Name:      "tree_height",
+		Help:      "The height of the tree",
+	},
+	[]string{"name"},
+)
+
+type BPTree[K any, V any] struct {
+	name       string
+	comparator func(K, K) int8
+	order      int
+	locker     sync.RWMutex
+	root       *atomic.Pointer[node[K, V]]
+	size       *atomic.Uint64
+}
+
+type node[K any, V any] struct {
+	parent   *node[K, V]
+	children []*node[K, V]
+	locker   sync.RWMutex
+	entries  []*entry[K, V]
+	right    *atomic.Pointer[node[K, V]]
+	left     *atomic.Pointer[node[K, V]]
+}
+
+type entry[K any, V any] struct {
+	locker sync.RWMutex
+	key    K
+	value  V
+}
+
+func NewBPTree[V any, K any](name string, order int, comparator func(K, K) int8) (*BPTree[K, V], error) {
+	if order < 2 {
+		return nil, errors.New("order must be greater than 2")
+	}
+
+	root := &atomic.Pointer[node[K, V]]{}
+	root.Store(&node[K, V]{
+		entries: []*entry[K, V]{},
+		right:   &atomic.Pointer[node[K, V]]{},
+		left:    &atomic.Pointer[node[K, V]]{},
+	})
+
+	t := &BPTree[K, V]{
+		name:       name,
+		comparator: comparator,
+		order:      order,
+		locker:     sync.RWMutex{},
+		root:       root,
+		size:       &atomic.Uint64{},
+	}
+
+	cacheMap[name] = t
+
+	return t, nil
+}
+
+func NewBPTreeWithOrdered[K constraints.Ordered, V any](name string, order int) (*BPTree[K, V], error) {
+	return NewBPTree[V](name, order, func(k1, k2 K) int8 {
+		switch {
+		case k1 < k2:
+			return -1
+		case k1 > k2:
+			return 1
+		default:
+			return 0
+		}
+	})
+}
+
+func NewBPTreeWithTime[V any](name string, order int) (*BPTree[time.Time, V], error) {
+	return NewBPTree[V](name, order, func(k1, k2 time.Time) int8 {
+		switch {
+		case k1.Before(k2):
+			return -1
+		case k1.After(k2):
+			return 1
+		default:
+			return 0
+		}
+	})
+}
+
+func (t *BPTree[K, V]) Purge() {
+	t.root.Store(&node[K, V]{
+		entries: []*entry[K, V]{},
+		right:   &atomic.Pointer[node[K, V]]{},
+		left:    &atomic.Pointer[node[K, V]]{},
+	})
+	t.size.Store(0)
+}
+
+func (t *BPTree[K, V]) Store(key K, value V) {
+	n, isEnd := t.root.Load(), false
+	for !isEnd {
+		func() {
+			locker := &n.locker
+			locker.Lock()
+			defer func() {
+				locker.Unlock()
+			}()
+
+			pos, found := t.search(n, key)
+			if found {
+				n.entries[pos].value = value
+				isEnd = true
+				return
+			}
+
+			if len(n.children) > 0 {
+				n = n.children[pos]
+				return
+			}
+
+			t.size.Add(1)
+			isEnd = true
+			e := &entry[K, V]{
+				key:   key,
+				value: value,
+			}
+
+			nd := n
+			for {
+				if len(nd.entries) < t.order-1 {
+					nd.entries = append(nd.entries, nil)
+					copy(nd.entries[pos+1:], nd.entries[pos:])
+					nd.entries[pos] = e
+					return
+				}
+
+				var left, right *node[K, V]
+				e, left, right = nd.insertSplit(pos, e)
+
+				if nd.parent == nil {
+					root := &node[K, V]{
+						entries:  []*entry[K, V]{e},
+						children: []*node[K, V]{left, right},
+						right:    &atomic.Pointer[node[K, V]]{},
+						left:     &atomic.Pointer[node[K, V]]{},
+					}
+					left.parent, right.parent = root, root
+					t.root.Store(root)
+					return
+				}
+
+				nd = nd.parent
+				nd.locker.Lock()
+				locker.Unlock()
+				locker = &nd.locker
+				pos, _ = t.search(nd, e.key)
+			}
+		}()
+	}
+}
+
+func (t *BPTree[K, V]) Load(key K) (V, bool) {
+	var v V
+	ok, isEnd := false, false
+	n := t.root.Load()
+	for !isEnd {
+		func() {
+			n.locker.RLock()
+			defer n.locker.RUnlock()
+
+			pos, found := t.search(n, key)
+			if found {
+				v = n.entries[pos].value
+				ok = true
+				return
+			}
+
+			if len(n.children) == 0 {
+				isEnd = true
+				return
+			}
+
+			n = n.children[pos]
+		}()
+
+		if ok {
+			return v, true
+		}
+	}
+
+	return v, false
+}
+
+func (t *BPTree[K, V]) search(n *node[K, V], key K) (int, bool) {
+	low, high := 0, len(n.entries)-1
+	var mid int
+	for low <= high {
+		mid = (low + high) / 2
+		compare := t.comparator(key, n.entries[mid].key)
+		switch {
+		case compare > 0:
+			low = mid + 1
+		case compare < 0:
+			high = mid - 1
+		default:
+			return mid, true
+		}
+	}
+
+	return low, false
+}
+
+func (n *node[K, V]) insertSplit(pos int, e *entry[K, V]) (*entry[K, V], *node[K, V], *node[K, V]) {
+	left := &node[K, V]{
+		parent:  n.parent,
+		entries: make([]*entry[K, V], 0, len(n.entries)),
+		right:   &atomic.Pointer[node[K, V]]{},
+		left:    &atomic.Pointer[node[K, V]]{},
+	}
+	right := &node[K, V]{
+		parent:  n.parent,
+		entries: make([]*entry[K, V], 0, len(n.entries)),
+		right:   &atomic.Pointer[node[K, V]]{},
+		left:    &atomic.Pointer[node[K, V]]{},
+	}
+
+	var parentEntry *entry[K, V]
+	middle := len(n.entries) / 2
+	count := 0
+	for i := 0; i < len(n.entries); i++ {
+		if i == pos {
+			switch {
+			case count < middle:
+				left.entries = append(left.entries, e)
+			case count == middle:
+				parentEntry = e
+				fallthrough
+			case count > middle:
+				right.entries = append(right.entries, e)
+			}
+			count++
+		}
+
+		switch {
+		case count < middle:
+			left.entries = append(left.entries, n.entries[i])
+		case count == middle:
+			parentEntry = n.entries[i]
+			fallthrough
+		case count > middle:
+			right.entries = append(right.entries, n.entries[i])
+		}
+		count++
+	}
+
+	if pos == len(n.entries) {
+		right.entries = append(right.entries, e)
+	}
+
+	if len(n.children) == 0 {
+		left.left.Store(n.left.Load())
+		left.right.Store(right)
+		right.left.Store(left)
+		right.right.Store(n.right.Load())
+
+		n.left.Load().right.Store(left)
+		n.right.Load().left.Store(right)
+	}
+
+	return parentEntry, left, right
+}
--- go.mod
+++ go.mod
@@ -67,6 +67,7 @@ require (
 	github.com/prometheus/client_model v0.3.0 // indirect
 	github.com/prometheus/common v0.37.0 // indirect
 	github.com/prometheus/procfs v0.8.0 // indirect
+	golang.org/x/exp v0.0.0-20230315142452-642cacee5cc0
 	golang.org/x/sys v0.6.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 )
